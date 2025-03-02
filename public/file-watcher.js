12580dfaca9c78df9f3ac18a2c03982d4ee995de
melihguleyupoglu <melihguleyup@gmail.com>
2025-03-02 14:35:01 +0300

    add watchFiles function

--- public/file-watcher.js
+++ public/file-watcher.js
@@ -1,1 +1,7 @@
-export default function watchFiles() {}
+import * as fs from "fs";
+export default function watchFiles() {
+  fs.watch("", (eventType, fileName) => {
+    console.log(`The file ${fileName} was modified`);
+    console.log(`The type of change was ${eventType}`);
+  });
+}
