148ebba09c0ba3598651cd2d24b946ddaaa72e41
melihguleyupoglu <melihguleyup@gmail.com>
2025-03-02 16:37:57 +0300

    fix: remove redundant reloads when nothing changed

--- public/file-watcher.js
+++ public/file-watcher.js
@@ -1,12 +1,30 @@
 import * as fs from "fs";
+import path from "path";
+
+let lastModifiedFiles = {};
+
 export default function watchFiles(ws) {
   fs.watch("./public", (eventType, fileName) => {
-    console.log(`The file ${fileName} was modified`);
-    console.log(`The type of change was ${eventType}`);
+    if (!fileName) return;
 
-    ws.clients.forEach((client) => {
-      if (client.readyState === client.OPEN) {
-        client.send(`File ${fileName} was updated. Reload the page!`);
+    const filePath = path.join("public", fileName);
+    fs.readFile(filePath, "utf-8", (err, currentContent) => {
+      if (err) {
+        console.error("Error reading file:", err);
+        return;
+      }
+
+      if (lastModifiedFiles[fileName] !== currentContent) {
+        console.log(`The file ${fileName} was modified`);
+        lastModifiedFiles[fileName] = currentContent;
+
+        ws.clients.forEach((client) => {
+          if (client.readyState === client.OPEN) {
+            client.send(`File ${fileName} was updated. Reload the page!`);
+          }
+        });
+      } else {
+        console.log(`No changes detected for ${fileName}`);
       }
     });
   });
