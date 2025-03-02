ed1dbe2cf4279451bad2176476c79af3cd399a42
melihguleyupoglu <melihguleyup@gmail.com>
2025-03-02 14:18:21 +0300

    feat: add ws client and make connection between client-server

---
+++ public/ws-client.js
@@ -0,0 +1,18 @@
+const socket = new WebSocket("ws://localhost:3000");
+
+socket.onopen = () => {
+  console.log("WebSocket connected.");
+  socket.send("Hello Server!");
+};
+
+socket.onmessage = (e) => {
+  console.log("Message from server:", e.data);
+};
+
+socket.onerror = (error) => {
+  console.error("WebSocket error:", error);
+};
+
+socket.onclose = () => {
+  console.log("WebSocket disconnected");
+};
