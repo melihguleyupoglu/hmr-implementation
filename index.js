ff02455c6d7be49b927ecb9525cda4e8e22ccfa4
melihguleyupoglu <melihguleyup@gmail.com>
2025-03-02 14:10:54 +0300

    feat: add websocket, http servers on index, add static serving public folder

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const express = require("express");
+import express from "express";
+import { createServer } from "http";
+import { WebSocketServer } from "ws";
+
 const app = express();
 const port = 3000;
+
+const server = createServer(app);
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function messsage(data) {
+    console.log(`received ${data}`);
+  });
+
+  ws.send("Hello from WebSocket!");
+});
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`HMR server is listening on port ${port}.`);
 });
