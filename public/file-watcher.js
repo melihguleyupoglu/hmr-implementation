35051351dda32e554ed631bdbb3a31e299fbb946
melihguleyupoglu <melihguleyup@gmail.com>
2025-03-02 14:41:22 +0300

    fix: fix the runtime error caused from watch function complaining about file path

--- public/file-watcher.js
+++ public/file-watcher.js
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 export default function watchFiles() {
-  fs.watch("", (eventType, fileName) => {
+  fs.watch("./", (eventType, fileName) => {
     console.log(`The file ${fileName} was modified`);
     console.log(`The type of change was ${eventType}`);
   });
